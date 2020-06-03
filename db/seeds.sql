a3fd11442cea88aa6504dcb63988eeb21bd8976c
Coletrainxd <tcgcolecarter@gmail.com>
2020-06-02 18:37:54 -0600

    first commit

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Baconator');
+INSERT INTO burgers (burger_name) VALUES ('Quarter Pounder');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Testing Burger', true);
+
